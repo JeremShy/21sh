0d3c26367f3cae184b2b8950ca9b418306f52006
Jeremy CAMHI <jcamhi@e2r1p12.42.fr>
2016-05-18 17:48:08 +0200

    Automatic Commit

--- boucle.c
+++ boucle.c
@@ -6,7 +6,7 @@
 /*   By: jcamhi <jcamhi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/01 19:52:28 by jcamhi            #+#    #+#             */
-/*   Updated: 2016/04/01 20:13:13 by jcamhi           ###   ########.fr       */
+/*   Updated: 2016/05/18 11:43:06 by jcamhi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,14 @@ void boucle(t_env *env)
 	char	buf[6];
 	int		r;
 
+	ft_bzero(buf, 6);
 	while ((r = read(0, buf, 5)))
 	{
 		env = NULL;
-		buf[r] = '\0';
-		if (isalpha(buf[0]) && buf[1] == '\0')
+		if ((ft_isalpha(buf[0]) || (buf[0] >= 32 && buf[0] <= 64) || (buf[0] >= 123 && buf[0] <= 126) || (buf[0] >= 91 && buf[0] <= 96)) && buf[1] == '\0')
 			ft_putchar(buf[0]);
 		else
-			ft_printf("%d - %d- %d - %d - %d - %d\n", buf[0], buf[1], buf[2], buf[3], buf[4], buf[5]);
+			ft_printf("%d - %d - %d - %d - %d - %d\n", buf[0], buf[1], buf[2], buf[3], buf[4], buf[5]);
+		ft_bzero(buf, 6);
 	}
 }
