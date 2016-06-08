e3423116665cb8fc0b4457550c7c4abc8b816198
Jeremy CAMHI <jcamhi@e2r2p21.42.fr>
2016-06-08 17:44:25 +0200

    Automatic Commit

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: jcamhi <jcamhi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/07 18:07:04 by jcamhi            #+#    #+#             */
-/*   Updated: 2016/06/07 22:23:58 by jcamhi           ###   ########.fr       */
+/*   Updated: 2016/06/08 16:46:15 by jcamhi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,35 @@ int main()
 	// if (close(i) == -1)
 	// 	exit(0);
 	//
+	int i;
+	int j;
+	int p1[2];
+	int p2[2];
+
+	// dup2(2, 1);
+	//
+	// dup2(2, 1);
+	// dup2(2, 1);
+//	close(1);
+// dup2(1, j);
+// dup2(1, i);
+	int p = fork();
+
+	if (p != 0)
+		wait(NULL);
+	else
+	{
+		dup2(0, 2);
+		dup2(1, 0);
+
+		// i = open("a", O_CREAT | O_WRONLY);
+		// j = open("b", O_CREAT | O_WRONLY);
+		// close(1);
+		// dup(i);
+		// dup2(i, j);
+		write(1, "poulet\n", sizeof("poulet\n"));
+		//write(2, "poulet\n", sizeof("poulet\n"));
+		exit(0);
+	}
 	return (0);
 }
