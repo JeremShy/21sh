02e510aeac223fa9f93c1833342b7bb679683035
Jeremy CAMHI <jcamhi@e2r2p18.42.fr>
2016-06-23 17:08:43 +0200

    Automatic Commit

--- quote.c
+++ quote.c
@@ -6,7 +6,7 @@
 /*   By: jcamhi <jcamhi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/14 16:22:40 by jcamhi            #+#    #+#             */
-/*   Updated: 2016/06/23 14:38:28 by jcamhi           ###   ########.fr       */
+/*   Updated: 2016/06/23 17:07:24 by jcamhi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,20 +73,34 @@ int 	is_quote_close(char car, char open)
 int		is_quote_end(t_data *data)
 {
 	size_t	i;
+	size_t	tmp;
 //	printf("\ncmd : [%s]\n", data->cmd);
 //	printf("data->c : %c\n", data->c);
 	i = ft_strlen(data->cmd) - data->real_len_cmd;
+	tmp = i;
+	printf("JE SUIS PASSER PAR ICI WESH\n");
 	while(data->cmd[i])
 	{
 		if (data->c == '\0')
 		{
 			if (is_quote_open(data->cmd[i]))
 				data->c = data->cmd[i];
-			else if (ft_strnstr(data->cmd + i, "<<", 2))
+		}
+		else if (is_quote_close(data->c, data->cmd[i]))
+			data->c = '\0';
+		i++;
+	}
+	if (data->c)
+		return (1);
+	i = data->end_hd;
+	printf("RIP LE HEREDOC [%s]\n", data->cmd + i);
+	while (data->c == '\0' && data->cmd[i])
+	{
+			if (ft_strnstr(data->cmd + i, "<<", 2))
 			{
 				i += 2;
 				data->c = '<';
-				data->end_hd = i - (ft_strlen(data->cmd) - data->real_len_cmd);
+				data->end_hd = i - tmp;
 				while (ft_isspace2(data->cmd[i]))
 					i++;
 				data->key_here = skip_quotes(data->cmd, &i, NULL); // On enleve les quotes.
@@ -95,13 +109,9 @@ int		is_quote_end(t_data *data)
 				data->index = 0;
 				printf("heredoc detected. key : %s\n", data->key_here);
 			}
-		}
-		else if (is_quote_close(data->c, data->cmd[i]))
-			data->c = '\0';
-		i++;
+			i++;
 	}
 	if (data->c)
 		return (1);
-	else
-		return (0);
+	return (0);
 }
