c520e5440e0bccbce2b2a91b80538aae94d29604
Jeremy CAMHI <jcamhi@e2r4p18.42.fr>
2016-07-07 19:46:58 +0200

    Commit d'office

--- boucle.c
+++ boucle.c
@@ -6,7 +6,7 @@
 /*   By: jcamhi <jcamhi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/30 15:30:12 by jcamhi            #+#    #+#             */
-/*   Updated: 2016/07/07 19:07:17 by jcamhi           ###   ########.fr       */
+/*   Updated: 2016/07/07 19:43:44 by jcamhi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -345,7 +345,7 @@ void	boucle(t_env *env, t_data *data)
 		}
 		else
 		{
-				ft_printf("%d - %d - %d - %d - %d - %d - cursor: x : %d, y : %d\n", buf[0], buf[1], buf[2], buf[3], buf[4], buf[5], data->curs_x, data->curs_y);
+				// ft_printf("%d - %d - %d - %d - %d - %d - cursor: x : %d, y : %d\n", buf[0], buf[1], buf[2], buf[3], buf[4], buf[5], data->curs_x, data->curs_y);
 		}
 		ft_bzero(buf, 6);
 	}
