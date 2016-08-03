36d59eca17123eb8bd74d1ee44bad18dc4742ea0
Jeremy CAMHI <jcamhi@e2r1p10.42.fr>
2016-08-03 16:23:56 +0200

    Put your commit message here

--- history_exit.c
+++ history_exit.c
@@ -6,7 +6,7 @@
 /*   By: jcamhi <jcamhi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/03 15:27:38 by jcamhi            #+#    #+#             */
-/*   Updated: 2016/08/03 15:29:28 by jcamhi           ###   ########.fr       */
+/*   Updated: 2016/08/03 16:03:46 by jcamhi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,28 @@
 
 void	history_exit(t_data *data)
 {
-	
+	char			*path;
+	int				fd;
+	t_history	*history;
+
+	if (get_history_path(data, &path) == 1)
+		return ;
+	fd = open(path, O_WRONLY | O_APPEND);
+	free(path);
+	if (fd == -1)
+		return ;
+	history = data->history;
+	while (history->prec)
+		history = history->prec;
+	while (history->next)
+	{
+		path = ft_itoa_base(history->time, 10);
+		path = ft_strjoinaf1(path, ";");
+		path = ft_strjoinaf1(path, history->line);
+		path = ft_strjoinaf1(path, "\n");
+		write(fd, path, ft_strlen(path));
+		free(path);
+		history = history->next;
+	}
+	close(fd);
 }
