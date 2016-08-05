77ddfbcb81fd8068cbe2858aa5da0e0d02e05a9d
Jeremy CAMHI <jcamhi@e2r1p8.42.fr>
2016-08-05 16:54:36 +0200

    lotocomplession elle marche lol

--- parse.c
+++ parse.c
@@ -153,8 +153,8 @@ t_cmd	*parse(char *str, t_hc *heredocs, t_env **env, t_data *data)
 	cmd = NULL; // On initialiase notre retour.
 	if (is_parse_error(str))
 		return (NULL);
-	if (find_replace_substitution(data, str))
-		return (NULL);
+	// if (find_replace_substitution(data, str))
+	// 	return (NULL);
 	data->history = add_history_elem(data->history, create_history_elem(data->cmd)); // On rajoute la ligne dans l'historique.
 	while (str[i])
 	{
