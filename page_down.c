69117c5555296a3430106ec67dbd42647f2b2250
Anna DOMINGUEZ <adomingu@e2r1p5.42.fr>
2016-08-25 19:22:19 +0200

    page_down ok and norme

--- page_down.c
+++ page_down.c
@@ -1,5 +1,17 @@
 #include <sh21.h>
 
+void	page_down_4(t_data *data)
+{
+	if (get_actual_cursor(data) == 0)
+	{
+		exec_tcap("le");
+		exec_tcap("nd");
+		data->index--;
+	}
+	else
+		move_left_without_mod(data);
+}
+
 void	page_down_3(t_data *data, int tmp)
 {
 	while (data->index <= tmp)
@@ -16,13 +28,13 @@ void	page_down_3(t_data *data, int tmp)
 void	page_down_2(t_data *data, int *tmp, int *why)
 {
 	if (data->index == data->index_min_copy && \
-			data->index_min_copy + data->win_x <= data->index_max_copy)
+		data->index_min_copy + data->win_x <= data->index_max_copy)
 	{
 		data->index_min_copy = data->index_min_copy + data->win_x;
 		*why = 1;
 	}
 	else if (data->index == data->index_min_copy && \
-			data->index_max_copy != data->index_min_copy)
+		data->index_max_copy != data->index_min_copy)
 	{
 		*tmp = data->index_min_copy;
 		data->index_min_copy = data->index_max_copy;
@@ -32,26 +44,23 @@ void	page_down_2(t_data *data, int *tmp, int *why)
 	{
 		data->index_max_copy += data->win_x;
 		if (data->index >= data->index_min_copy && \
-				data->index <= data->index_max_copy)
+			data->index <= data->index_max_copy)
 			exec_tcap("mr");
 	}
 }
 
 void	page_down_1(t_data *data)
 {
-	if (data->mode_copy == 0)
+	if (get_actual_cursor(data) == 0)
 	{
-		if (get_actual_cursor(data) == 0)
-		{
-			write(1, data->cmd + data->index, data->win_x + 1);
-			data->index += data->win_x + 1;
-			move_left_without_mod(data);
-		}
-		else
-		{
-			write(1, data->cmd + data->index, data->win_x);
-			data->index += data->win_x;
-		}
+		write(1, data->cmd + data->index, data->win_x + 1);
+		data->index += data->win_x + 1;
+		move_left_without_mod(data);
+	}
+	else
+	{
+		write(1, data->cmd + data->index, data->win_x);
+		data->index += data->win_x;
 	}
 }
 
@@ -60,24 +69,23 @@ void	page_down(t_data *data)
 	int	tmp;
 	int why;
 
-	if ((data->index + data->win_x < (int)ft_strlen(data->cmd)
-				|| (get_actual_cursor(data) != 0 && \
-					data->index + data->win_x == (int)ft_strlen(data->cmd)))\
-			&& !(data->mode_copy && \
-				data->index + data->win_x == (int)ft_strlen(data->cmd)))
+	why = 0;
+	tmp = 0;
+	if ((data->index + data->win_x < (int)ft_strlen(data->cmd)\
+	|| (get_actual_cursor(data) != 0 && \
+	data->index + data->win_x == (int)ft_strlen(data->cmd))) && \
+	!(data->mode_copy && \
+	data->index + data->win_x == (int)ft_strlen(data->cmd)))
 	{
-		why = 0;
-		page_down_1(data);
-	}
-	else
-	{
-		page_down_2(data, &tmp, &why);
-		tmp = data->index + data->win_x;
-		page_down_3(data, tmp);
-		why ? exec_tcap("me") : 0;
-		if (get_actual_cursor(data) == 0)
-			move_index(data);
+		if (data->mode_copy == 0)
+			page_down_1(data);
 		else
-			move_left_without_mod(data);
+		{
+			page_down_2(data, &tmp, &why);
+			tmp = data->index + data->win_x;
+			page_down_3(data, tmp);
+			why ? exec_tcap("me") : 0;
+			page_down_4(data);
+		}
 	}
 }
