f4f84ee1a4b85e539996a5c7e3462f61fd63b21a
Jeremy CAMHI <jcamhi@e2r4p22.42.fr>
2016-08-29 16:04:32 +0200

    resolvage d'un invalid read

--- is_parse.c
+++ is_parse.c
@@ -5,19 +5,16 @@ int		is_aggr(size_t *i, char *str, int jump)
 	size_t tmp;
 
 	tmp = *i;
-	if ((str[*i] == '>' || str[*i] == '<') && str[*i + 1] == '&' &&\
-			(ft_isdigit(str[*i + 2]) || str[*i + 2] == '-'))
+	if ((str[*i] == '>' || str[*i] == '<') && str[*i + 1] == '&' && (ft_isdigit(str[*i + 2]) || str[*i + 2] == '-')) // Il faut que ca commence par un <, suivi de..
 	{
 		(*i) += 2;
 		while (ft_isdigit(str[*i]) || str[*i] == '-')
 			(*i)++;
-		if (!jump)
+		if (!jump) // Si on a passé un en troisieme parametre, on fait bouger i.
 			*i = tmp;
 		return (1);
 	}
-	else if (ft_isdigit(str[*i]) && (str[*i + 1] == '>' ||\
-				str[*i + 1] == '<') && str[*i + 2] == '&' && \
-			(ft_isdigit(str[*i + 3]) || str[*i + 3] == '-'))
+	else if (ft_isdigit(str[*i]) && (str[*i + 1] == '>' || str[*i + 1] == '<') && str[*i + 2] == '&' && (ft_isdigit(str[*i + 3]) || str[*i + 3] == '-')) // Ou alors ca peut commencer par un nombre..
 	{
 		(*i) += 3;
 		while (ft_isdigit(str[*i]) || str[*i] == '-')
@@ -29,46 +26,40 @@ int		is_aggr(size_t *i, char *str, int jump)
 	return (0);
 }
 
-size_t	is_redir_move_tmp(size_t j, char *str)
-{
-	size_t		i;
-
-	i = 0;
-	if ((str[i] == '<' && str[i + 1] == '<') ||\
-			(str[i] == '>' && str[i + 1] == '>'))
-		i += 2;
-	else if (str[i] == '<' || str[i] == '>')
-		i++;
-	else if (ft_isdigit(str[j]))
-	{
-		if ((str[i + 1] == '>' && str[i + 2] == '>'))
-			i += 3;
-		else if (str[i + 1] == '>')
-			i += 2;
-	}
-	return (i);
-}
-
-char	*is_redir(size_t *i, char *str, int jump, t_cmd *cmd)
+char	*is_redir(size_t *i, char *str, int jump, t_cmd	*cmd)
 {
 	size_t	tmp;
+	char		*quote;
+	(void)		quote;
 
 	tmp = *i;
-	tmp += is_redir_move_tmp(*i, str + tmp);
-	if (tmp != *i)
+	if ((str[tmp] == '<' && str[tmp + 1] == '<') || (str[tmp] == '>' && str[tmp + 1] == '>')) // Il faut commence par deux chevrons...
+		tmp += 2;
+	else if (str[tmp] == '<' || str[tmp] == '>') // ... ou par un seul...
+		tmp++;
+	else if (ft_isdigit(str[*i])) // ... ou par un chiffre
 	{
-		while (ft_isspace2(str[tmp]))
+		// if (ft_isdigit(str[*i + 1]))
+		// 	return (NULL);
+		if ((str[tmp + 1] == '>' && str[tmp + 2] == '>'))
+			tmp += 3;
+		else if (str[tmp + 1] == '>')
+			tmp += 2;
+	}
+	if (tmp != *i) // Si tmp a bougé du fait des opérations précédentes.
+	{
+		while (ft_isspace2(str[tmp])) // On saute les espaces
 			tmp++;
-		if (is_empty(str, &tmp))
+		if (is_empty(str, &tmp)) // Si la chaine est vide, on parse error (ls <           			)
 		{
 			if (cmd)
 				cmd->p_error = 1;
 			return (NULL);
 		}
-		skip_quotes_nb_arg(str, &tmp, cmd);
-		if (jump)
+		quote = skip_quotes_nb_arg(str, &tmp, cmd); // On enleve les quotes.
+ 		if (jump)
 			*i = tmp;
-		return ("KAKA");
+		return ("KAKA"); // On se barre à la fin.
 	}
 	return (NULL);
 }
@@ -78,11 +69,12 @@ int		is_sep(size_t *i, char *str, int jump, t_cmd *cmd)
 	size_t	ret;
 
 	ret = *i;
+	// printf("on appelle is sep avec str[i] = : %s\n", str + *i);
 	if (ft_strnequ(str + *i, "||", 2))
 		ret += 2;
 	else if (ft_strnequ(str + *i, "&&", 2))
 		ret += 2;
-	else if (str[*i] == ';')
+	else if (str[*i] == ';') // Si c'est un ;, un | ou un &&, on augmente ret.
 		ret++;
 	else if (str[*i] == '|')
 		ret++;
@@ -91,8 +83,9 @@ int		is_sep(size_t *i, char *str, int jump, t_cmd *cmd)
 	if (is_escaped_char(str, *i))
 		return (0);
 	if (cmd)
-		cmd->sep = def_sep(str + *i);
-	if (jump)
+		cmd->sep = def_sep(str + *i); // On mets le bon sep.
+	if (jump) // Si le troisieme parametre est a un, on saute le sep.
 		*i = ret;
+	// printf("on renvoie 1\n");
 	return (1);
 }
