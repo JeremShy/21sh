284ee153c789c2e4c7baa8e1ebf556faf0eeeed0
Jeremy CAMHI <jcamhi@e2r3p17.42.fr>
2016-09-03 23:30:01 +0200

    Projet fini ?

--- includes/sh21.h
+++ includes/sh21.h
@@ -6,7 +6,7 @@
 /*   By: jcamhi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/01 19:47:59 by jcamhi            #+#    #+#             */
-/*   Updated: 2016/09/03 17:46:56 by adomingu         ###   ########.fr       */
+/*   Updated: 2016/09/03 23:25:57 by jcamhi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -240,7 +240,6 @@ void					display_heredoc (t_hc *elem);
 void					free_heredoc(t_data *data, t_hc *list);
 char					*find_exec(char *scmd, t_data *data, t_env *env);
 int						fork_pipes(t_cmd *cmd, t_env *env, t_data *data);
-char					*get_pb(void);
 int						is_empty_border(char *str, size_t beg, size_t end);
 void					get_pos_after_quote(size_t *i, char *str);
 void					close_fd_cmd(t_cmd *cmd);
